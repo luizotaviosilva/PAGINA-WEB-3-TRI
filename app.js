2355abadfd2d60319174be0fe8a828a53432e2ab
luiz.vj7 <144828970+luizotaviosilva@users.noreply.github.com>
2024-10-07 15:42:18 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
@@ -15,6 +14,16 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
     container.appendChild(cartao)
 
 }
